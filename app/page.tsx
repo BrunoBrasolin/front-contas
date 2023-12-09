2a9d62ec0d617c5c7b1ed7d4f928dc09cd377d46
Bruno Brasolin <brunobrasolinc@gmail.com>
2023-12-08 22:12:33 -0300

    UP | Calculo porcentagem

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useState, FormEvent } from 'react';
 
 export default function Home() {
 	const [percentageValue, setPercentageValue] = useState<number | ''>('');
+	const [resultPercentage, setResultPercentage] = useState<{ Leticia: number; Bruno: number } | null>(null);
 	const [salary, setSalary] = useState<number | ''>('');
 	const [selectedPerson, setSelectedPerson] = useState('');
 
@@ -11,66 +12,50 @@ export default function Home() {
 		e.preventDefault();
 
 		try {
-			console.log(percentageValue);
-			const response = await fetch('https://localhost:32770/api/conta?valor=123', {
+			const response = await fetch(`http://168.75.82.21:82/api/conta?valor=${percentageValue}`, {
 				method: 'GET',
 				headers: {
 					'Content-Type': 'application/json',
 				},
 			});
 
-			console.log(response);
+			let data = await response.json();
+
+			setResultPercentage(data);
 		} catch (error) {
-			console.error('Error:', error);
+			alert(`Erro, fazor entrar em contato com o suporte: ${error}`);
 		}
 	};
 
-	const handleUpdateSalarySubmit = async (e: FormEvent) => {
-		e.preventDefault();
-
-		try {
-			const response = await fetch('http://localhost:4430/api/save', {
-				method: 'POST',
-				headers: {
-					'Content-Type': 'application/json',
-				},
-				body: JSON.stringify({ value: salary, person: selectedPerson }),
-			});
-
-			console.log(response);
-		} catch (error) {
-			console.error('Error:', error);
-		}
+	const handleClear = () => {
+		setResultPercentage(null);
+		setPercentageValue('');
 	};
 
 	return (
 		<main>
 			<div className="max-w-md mx-auto mt-8">
-				<form className="mb-8" onSubmit={handlePercentageSubmit}>
+				<form className="mb-6" onSubmit={handlePercentageSubmit}>
 					<label className="block text-sm font-medium">
 						Cálculo porcentagem:
 						<input type="number" value={percentageValue} onChange={e => setPercentageValue(parseInt(e.target.value, 10) || '')} className="mt-1 p-2 border rounded-md w-full text-green-950" />
 					</label>
-					<button type="submit" className="mt-2 px-4 py-2 bg-teal-500 text-white rounded-md">
-						Submit
-					</button>
+
+					<div className="flex justify-between mt-5">
+						<button type="submit" className="px-4 py-2 rounded-md bg-teal-500 text-white">
+							Calcular
+						</button>
+						<button type="button" onClick={handleClear} className="ml-2 px-4 py-2 border rounded-md text-teal-500 border-teal-500">
+							Limpar
+						</button>
+					</div>
 				</form>
 
-				<form onSubmit={handleUpdateSalarySubmit}>
-					<label className="block text-sm font-medium">
-						Atualizar salário:
-						<input type="number" value={salary} onChange={e => setSalary(parseInt(e.target.value, 10) || '')} className="mt-1 p-2 border rounded-md w-full text-green-950" />
-					</label>
-					<label className="block mt-4 text-sm font-medium text-green-950">
-						<select value={selectedPerson} onChange={e => setSelectedPerson(e.target.value)} className="mt-1 p-2 border rounded-md w-full">
-							<option value="0">Leticia</option>
-							<option value="1">Bruno</option>
-						</select>
-					</label>
-					<button type="submit" className="mt-4 px-4 py-2 bg-teal-500 text-white rounded-md">
-						Submit
-					</button>
-				</form>
+				<div className="mb-12">
+					<h2>Valor de cada um:</h2>
+					<pre>Letícia: R${resultPercentage?.Leticia || '0,00'}</pre>
+					<pre>Bruno: R${resultPercentage?.Bruno || '0,00'}</pre>
+				</div>
 			</div>
 		</main>
 	);
